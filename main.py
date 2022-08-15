f40692af25ee4ea04109cbef131a382c9da7eebf
DimonTer <dr.passkuda@gmail.com>
2022-08-15 10:19:34 +0300

    added bot to main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from config import token
+import telebot
+
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler()
+def func(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
